e1ec8d09433413dffae8748df222229300585e55
JWKelly29 <jwkelly29@gmail.com>
2017-12-23 15:15:03 +0000

    Three stacks setup

--- stacks_and_queues.py
+++ stacks_and_queues.py
@@ -19,6 +19,40 @@ class Stack:
          return len(self.items)
 
 
+class ThreeStacks:
+	def __init__(self, size):
+		self.items = [None] * size
+		self.stack_size = size / 3
+
+		self.bottom_first = 0
+		self.end_first = stack_size - 1
+		self.start_middle = stack_size
+		self.end_middle = (stack_size * 2) - 1
+		self.start_last = stack_size * 2
+		self.end_last = size - 1
+
+		self.stack1_idx = 0
+		self.stack2_idx = self.stack1_idx + stack_size
+		self.stack3_idx = self.stack2_idx + stack_size
+
+		
+		
+
+	def push(self, item):
+
+		
+
+	def pop(self):
+		
+
+	def size(self):
+		
+
+
+
+
+
+
 s = Stack()
 
 s.push(1)
@@ -32,6 +66,22 @@ print "created Stack"
 ### Questions
 
 #### Describe how you could use a single array to implement three stacks
+
+ts = ThreeStacks()
+ts.push(1)
+ts.push(2)
+ts.push(3)
+ts.push(4)
+ts.push(5)
+ts.push(6)
+ts.push(7)
+ts.push(8)
+ts.push(9)
+
+print("size of three stack", ts.size)
+print("last item added to three stack", ts.peek_last)
+
+
 
 #### Describe a stack in which in addition to push and pop function also has a minimum function.
 #### The push, pop and minimum functions should all operate in O(1) time.
